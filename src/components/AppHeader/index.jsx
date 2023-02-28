f6c8b04a05b159b052c98939cc2f1f99b9dfd448
Melaanie <melaanie.ailleaume@gmail.com>
2023-02-28 10:41:13 +0100

    Rectification class en className dans AppHeader

--- src/components/AppHeader/index.jsx
+++ src/components/AppHeader/index.jsx
@@ -10,7 +10,7 @@ const AppHeader = ({ isLogged, setIsLogged }) => {
 
   return (
     <>
-     <div class="header__title-container"></div>
+     <div className="header__title-container"></div>
       <header className="header__title-container">
         <a href="/">
           <img src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/013ad70b-7c62-464c-9201-9a96392e6737/logo_odelices.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230227%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230227T162042Z&X-Amz-Expires=86400&X-Amz-Signature=96b98c88d4c395ee55e89e2bc3a681a12e96d5c3d481c42a10bf1262a01bb693&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22logo_odelices.png%22&x-id=GetObject" 

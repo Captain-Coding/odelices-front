db28c3400c7880e7089ca33c830797daf39b135a
Captain-Coding <captaincoding@pm.me>
2023-02-28 09:49:21 +0100

    :art: feat: add logo header

--- src/components/AppHeader/index.jsx
+++ src/components/AppHeader/index.jsx
@@ -13,14 +13,14 @@ const AppHeader = ({ isLogged, setIsLogged }) => {
      <div class="header__title-container"></div>
       <header className="header__title-container">
         <a href="/">
-          <img src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/013ad70b-7c62-464c-9201-9a96392e6737/logo_odelices.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230227%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230227T162042Z&X-Amz-Expires=86400&X-Amz-Signature=96b98c88d4c395ee55e89e2bc3a681a12e96d5c3d481c42a10bf1262a01bb693&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22logo_odelices.png%22&x-id=GetObject" 
+          <img src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/883745b3-a6f9-4bba-ae0b-acc875ed3e6c/Untitled.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230228%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230228T084725Z&X-Amz-Expires=86400&X-Amz-Signature=2c40e5cda5e8381e567911840cbb6b1c9b0c4894f1cad45c7461bd1c14f49920&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22Untitled.png%22&x-id=GetObject" 
           alt="logo O'délices" 
           className= "logo"
         />
          </a>
 
          <a href="/">
-          <img src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/02123ab9-8505-4b51-a3c0-d71d1f50fb3a/image_%282%29.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230227%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230227T162543Z&X-Amz-Expires=86400&X-Amz-Signature=a1bf31bc9dabd383f74b2225c0ce0d4d653f1abf1821fafcffca3e5c3e14ef44&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22image%2520%282%29.png%22&x-id=GetObject" 
+          <img src="https://s3.us-west-2.amazonaws.com/secure.notion-static.com/013ad70b-7c62-464c-9201-9a96392e6737/logo_odelices.png?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=AKIAT73L2G45EIPT3X45%2F20230228%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20230228T084810Z&X-Amz-Expires=86400&X-Amz-Signature=40944b99746b21899d8a0f445333bb76e5b8ca87e8ae7092875046b15123a5b0&X-Amz-SignedHeaders=host&response-content-disposition=filename%3D%22logo_odelices.png%22&x-id=GetObject" 
           alt="logo O'Delices"
           className= "logo"
           />

aa826c283f9993aabb2170e1e715d9f66b5ad11b
Melaanie <melaanie.ailleaume@gmail.com>
2023-03-03 17:33:12 +0100

    lien cliquable sur chaque recette et redirection sur details de la recette

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,10 @@
 import SearchBar from "../components/SearchBar";
+import { Link } from "react-router-dom";
 import { Card, Image, Rating } from "semantic-ui-react";
 import "semantic-ui-css/semantic.min.css";
 import { useState, useEffect } from 'react';
 import '../App.css';
-import {getAPI} from '../utils/api';
+import { getAPI } from '../utils/api';
 
 const HomePage = () => {
   const [recipeList, setRecipeList] = useState([])
@@ -18,11 +19,34 @@ const HomePage = () => {
   return (
     <>
       <SearchBar recipeList={recipeList} SetSearchResult={SetSearchResult} />
-    <div className="card_container" >
-      {searchResult.length < 1 &&
-        recipeList.map((recipe, index) => (
+      <div className="card_container" >
+        {searchResult.length < 1 &&
+          recipeList.map((recipe, index) => (
+            <div key={index}>
+              <Link to={`/recipes/${recipe.id}`}>
+                <Card>
+                  <Image src={recipe.picture} wrapped ui={false} />
+                  <Card.Content>
+                    <Card.Header>{recipe.name}</Card.Header>
+                    <Card.Description>{recipe.pseudo}</Card.Description>
+                  </Card.Content>
+                  <Card.Content extra>
+                    <Rating
+                      icon="star"
+                      defaultRating={Math.round(recipe.avg_rate)}
+                      maxRating={5}
+                      disabled
+                    />
+                  </Card.Content>
+                </Card>
+              </Link>
+            </div>
+          ))
+        }
+        {searchResult && searchResult.map((recipe, index) => (
           <div key={index}>
-            <Card>
+            <Link to={`/recipes/${recipe.id}`}>
+          <Card>
               <Image src={recipe.picture} wrapped ui={false} />
               <Card.Content>
                 <Card.Header>{recipe.name}</Card.Header>
@@ -31,36 +55,17 @@ const HomePage = () => {
               <Card.Content extra>
                 <Rating
                   icon="star"
-                  defaultRating={Math.round(recipe.avg_rate)}
+                  defaultRating={Math.round(recipe.average_rating)}
                   maxRating={5}
                   disabled
                 />
               </Card.Content>
             </Card>
+            </Link>
           </div>
-        ))
-      }
-      {searchResult && searchResult.map((recipe, index) => (
-        <div key={index}>
-          <Card>
-            <Image src={recipe.picture} wrapped ui={false} />
-            <Card.Content>
-              <Card.Header>{recipe.name}</Card.Header>
-              <Card.Description>{recipe.pseudo}</Card.Description>
-            </Card.Content>
-            <Card.Content extra>
-              <Rating
-                icon="star"
-                defaultRating={Math.round(recipe.average_rating)}
-                maxRating={5}
-                disabled
-              />
-            </Card.Content>
-          </Card>
-        </div>
 
-      ))}
-  </div>
+        ))}
+      </div>
     </>
   );
 };

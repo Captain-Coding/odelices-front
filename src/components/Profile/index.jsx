e19dcd9b6f644850e683b45ffabf80f6cf0eb9f1
OUZZMANSofiane <o.sofiane@hotmail.fr>
2023-03-08 14:24:40 +0100

    handleDeleteProfil fonctionnelle

--- src/components/Profile/index.jsx
+++ src/components/Profile/index.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { getAPI } from "../../utils/api";
 import jwt_decode from "jwt-decode";
 
@@ -7,63 +7,79 @@ import jwt_decode from "jwt-decode";
 
 
 const Profile = () => {
-    // const [Firstname, setFirstname] = useState()
-    // const [Lastname, setLastname] = useState()
-    // const [Pseudo, setPseudo] = useState()
-    const [member, setMember] = useState({});
-    
-    const decoded = jwt_decode(localStorage.token);
-    const id = decoded.id
+  // const [Firstname, setFirstname] = useState()
+  // const [Lastname, setLastname] = useState()
+  // const [Pseudo, setPseudo] = useState()
+  const [member, setMember] = useState({});
+  const navigate = useNavigate();
+  const decoded = jwt_decode(localStorage.token);
+  const id = decoded.id
 
 
-    useEffect(() => {
-        getAPI().get(`/members/${id}`)
-            .then(response => {
-                setMember(response.data)
-                console.log(response.data)
-            })
-            .catch(error => console.log(error))
-    }, [])
+  useEffect(() => {
+    getAPI().get(`/members/${id}`)
+      .then(response => {
+        setMember(response.data)
+        console.log(response.data)
+      })
+      .catch(error => console.log(error))
+  }, [])
 
 
+  const handleDeleteProfil = () => {
+    const confirmed = window.confirm("Etes-vous sûr de vouloir supprimer votre profil ?");
+    try {
+      if (confirmed) {
+        getAPI().delete(`/members/${id}`, { headers: { Authorization: `Bearer ${localStorage.token}` } })
+          .then(response => {
+            console.log(response.data)
+            localStorage.removeItem('token');
+          })
+        navigate("/");
+      }
+    } catch (error) {
+      console.log("error", error);
+    }
+  }
 
-    return (
-        <>
-            <div className="PictureProfil">
-                <img src={member.picture} alt={member.pseudo} />
-            </div>
 
-            <div className="informations">
+  return (
+    <>
+      <div className="PictureProfil">
+        <img src={member.picture} alt={member.pseudo} />
+      </div>
 
-                <button>Suprrimer votre profil</button>
-                <h1>Mes informations</h1>
-                <p>Nom : {member.lastname}</p>
-                <p>Prénom : {member.firstname}</p>
-                <p>Pseudo : {member.pseudo}</p>
-                <button className="deconnexion" type="submit">Se déconnecter</button>
-                <div className="myRecipe">
-                    <h2>Mes recettes</h2>
-                    <Link to={`/recipes/create`}>
-                        <button>Ajouter une recette</button>
-                    </Link>
+      <div className="informations">
 
-                </div>
-            </div>
+        <button onClick={handleDeleteProfil}>Suprrimer votre profil</button>
+        <h1>Mes informations</h1>
+        <p>Nom : {member.lastname}</p>
+        <p>Prénom : {member.firstname}</p>
+        <p>Pseudo : {member.pseudo}</p>
+        <button className="deconnexion" type="submit">Se déconnecter</button>
+        <div className="myRecipe">
+          <h2>Mes recettes</h2>
+          <Link to={`/recipes/create`}>
+            <button>Ajouter une recette</button>
+          </Link>
 
-            <div className="modification">
-                <h2>Modifie tes informations</h2>
-                <form action="" method="post">
-                    <input type="text" placeholder="Saisissez votre Nom" />
-                    <button type="submit">Modifier</button>
-                    <input type="text" placeholder="Saisissez votre Prénom" />
-                    <button type="submit">Modifier</button>
-                    <input type="text" placeholder="Saisissez votre Pseudo" />
-                    <button type="submit">Modifier</button>
-                </form>
-            </div>
+        </div>
+      </div>
 
-        </>
-    )
+      <div className="modification">
+        <h2>Modifie tes informations</h2>
+        <form action="" method="post">
+          <input type="text" placeholder="Saisissez votre Nom" />
+          <button type="submit">Modifier</button>
+          <input type="text" placeholder="Saisissez votre Prénom" />
+          <button type="submit">Modifier</button>
+          <input type="text" placeholder="Saisissez votre Pseudo" />
+          <button type="submit">Modifier</button>
+        </form>
+      </div>
+
+    </>
+  )
 
 }
 

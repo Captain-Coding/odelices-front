881a5dff1349fa05e807371bd347ab3b5a22b7a0
OUZZMANSofiane <o.sofiane@hotmail.fr>
2023-03-08 10:03:08 +0100

    save app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import { useState,useEffect } from "react";
-import {getToken} from "./utils/api";
+import { useState, useEffect } from "react";
+import { getToken } from "./utils/api";
 import jwt_decode from "jwt-decode";
 
 // == Import page
@@ -13,7 +13,7 @@ import Signup from "./pages/SignUp/SignUp";
 import Recipe from "./pages/Recipe";
 import SignIn from "./pages/SignIn";
 import CreateRecipe from "./pages/CreateRecipe";
-import UpdateRecipe from  "./pages/UpdateRecipe";
+import UpdateRecipe from "./pages/UpdateRecipe";
 import AllRecipes from "./pages/AllRecipes";
 import Profile from "./pages/Profile";
 import Error from "./pages/Error";
@@ -29,39 +29,36 @@ import SearchBar from "./components/SearchBar";
 const App = () => {
   const [isLogged, setIsLogged] = useState(false);
   const [token, setToken] = useState(null);
-  useEffect(()=>{
+  useEffect(() => {
     let token = getToken()
-    if(token && typeof token == "string"){
-      let body = token.split(".")[1]
-      let decrypted=JSON.parse(atob(body))
-      if(decrypted.exp*1000 > Date.now){
-        console.log("token is expired, we don't log user. ");
-      }else{
-        console.log("token valid, we should log user. ")
-        setIsLogged(true);
-        setToken(token);
-      }
+    if (token && typeof token == "string") {
+
+      let decrypted = jwt_decode(token)
+
+      setIsLogged(true);
+      setToken(token);
+
     }
-  },[])
+  }, [])
   console.log(isLogged, "isLoggedFromHome? ")
   return (
     <>
       <Router>
-      <AppHeader isLogged={isLogged} setIsLogged={setIsLogged} />
-      
+        <AppHeader isLogged={isLogged} setIsLogged={setIsLogged} />
+
         <Routes>
           <Route path="/" element={<Homepage />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/recipes/:id" element={<Recipe />} />
           <Route path="/profile" element={<Profile />} />
-          <Route path="/signin" element={<SignIn setIsLogged={setIsLogged}/>} />
+          <Route path="/signin" element={<SignIn setIsLogged={setIsLogged} />} />
           <Route path="/recipes/create" element={<CreateRecipe />} />
           <Route path="/*" element={<Error />} />
           <Route path="/recipes" element={<AllRecipes />} />
           <Route path="/CGU" element={<CGU />} />
           <Route path="/recipes/update/:id" element={<UpdateRecipe />} />
         </Routes>
-      <AppFooter />
+        <AppFooter />
       </Router>
 
     </>
